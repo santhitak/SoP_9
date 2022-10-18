90498c565071a0a07c9efa206cffaeea06bbdba4
santhitak <santhita.sk@gmail.com>
2022-10-18 11:19:06 +0700

    finished

---
+++ src/main/java/com/example/productservice/event/ProductCreatedEvent.java
@@ -0,0 +1,18 @@
+package com.example.productservice.event;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductCreatedEvent {
+
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+    public ProductCreatedEvent() {
+
+    }
+}
